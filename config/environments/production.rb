823564c90255ee6b9886e16c62d21a061f7f05c7
RaphaelDeLaGhetto <daniel@capitolhill.ca>
2015-08-27 10:16:42 -0600

    Configure default URL options for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Devise
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
-
-
+  config.action_mailer.default_url_options = { :host => ENV['host'] }
 end
