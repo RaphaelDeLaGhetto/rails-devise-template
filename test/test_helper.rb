287defa85a3cefb1ec95c6420ff1fe2db28fcd6b
RaphaelDeLaGhetto <daniel@capitolhill.ca>
2015-08-29 16:32:04 -0600

    Get passing password reset integration tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,8 @@ require 'devise'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+
+  include ApplicationHelper
 end
 
 # As per https://github.com/plataformatec/devise#test-helpers
